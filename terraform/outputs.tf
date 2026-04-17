39e876a6b7fe5e25bab7f48cc8fd55e1842e14cc
Aurelien Kumarathas <akumarathas27@gmail.com>
2026-04-17 16:24:57 +0100

    feat: add outputs for key resource identifiers

---
+++ terraform/outputs.tf
@@ -0,0 +1,25 @@
+output "vpc_id" {
+  description = "ID of the QuantumTrade VPC"
+  value       = aws_vpc.main.id
+}
+
+output "app_server_id" {
+  description = "EC2 instance ID of the application server"
+  value       = aws_instance.app_server.id
+}
+
+output "data_bucket_arn" {
+  description = "ARN of the transaction data S3 bucket"
+  value       = aws_s3_bucket.data_bucket.arn
+}
+
+output "db_instance_endpoint" {
+  description = "RDS PostgreSQL connection endpoint"
+  value       = aws_db_instance.main.endpoint
+  sensitive   = true
+}
+
+output "private_subnet_id" {
+  description = "ID of the primary private subnet"
+  value       = aws_subnet.private.id
+}
